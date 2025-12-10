156dfe561c1a51589b38ed2efdeb6bde01028ee6
gmach <gmach@student.42lyon.fr>
2025-12-10 14:34:49 +0100

    first implemeentation of main for testing purposes. little update on lstaddback

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: gmach <gmach@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 07:11:39 by bfitte/gmac       #+#    #+#             */
-/*   Updated: 2025/12/10 13:17:10 by gmach            ###   ########lyon.fr   */
+/*   Updated: 2025/12/10 14:32:33 by gmach            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -38,25 +38,25 @@ void	delete_value(int value)
 	(void)value;
 }
 
-t_stack	**parse_input(int argc, char **argv)
+t_stack	*parse_input(int nb_input, char **numbers)
 {
 	int		i;
-	t_stack	**stack_a;
+	t_stack	*stack_a;
 	t_stack	*new_node;
 	int		value;
 
 	stack_a = NULL;
-	i = 1;
-	while (i < argc)
+	i = 0;
+	while (i < nb_input)
 	{
-		value = ft_atoi(argv[i]);
+		value = ft_atoi(numbers[i]);
 		new_node = ft_lstnew(value);
 		if (!new_node)
 		{
-			ft_lstclear(stack_a, delete_value);
+			ft_lstclear(&stack_a, delete_value);
 			return (NULL);
 		}
-		ft_lstadd_back(stack_a, new_node);
+		ft_lstadd_back(&stack_a, new_node);
 		i++;
 	}
 	return (stack_a);
@@ -66,21 +66,37 @@ t_stack	**parse_input(int argc, char **argv)
 int	main(int argc, char **argv)
 {
 	int	i;
-	t_stack	**stack_a;
-	t_stack	**stack_b;
+	t_stack	*stack_a;
+	t_stack	*stack_b;
 	t_flags flags;
 
+	ft_bzero(&flags, sizeof(t_flags));
 	flags.adaptative = 1;
 	i = 1;
 	stack_b = NULL;
 	if (argc < 2)
 		return (0);
+	ft_printf("check\n");
 	while (argv[i][0] == '-' && argv[i][1] == '-')
 		check_flags(argv[i++], &flags);
-	stack_a = parse_input(argc - i + 1, &argv[i - 1]);
+	ft_printf("Flags - simple: %d, medium: %d, complex: %d, adaptative: %d, bench_mode: %d\n",
+		flags.simple, flags.medium, flags.complex, flags.adaptative, flags.bench_mode);
+	stack_a = parse_input(argc - i, &argv[i]);
 	if (!stack_a)
 		return (1);
-	if (flags.simple == 1)
-		simple_sort(stack_a, stack_b);
+	if (flags.simple)
+		simple_sort(&stack_a, &stack_b);
+	// else if (flags.medium)
+	// 	medium_sort(&stack_a, &stack_b);
+	// else if (flags.complex)
+	// 	complex_sort(&stack_a, &stack_b);
+	ft_printf("Stack A:\n");
+	t_stack *current = stack_a;
+	while (current != NULL)
+	{
+		ft_printf("%d\n", current->value);
+		current = current->next;
+	}
+	ft_lstclear(&stack_a, delete_value);
 	return (0);
 }
