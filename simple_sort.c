b4700b96afee9231394e0b2aaeae7bdef588c28c
gmach <gmach@student.42lyon.fr>
2025-12-11 17:55:58 +0100

    some test to implement insertion method as simple algo

--- simple_sort.c
+++ simple_sort.c
@@ -6,7 +6,7 @@
 /*   By: gmach <gmach@student.42lyon.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/08 19:07:25 by gmach             #+#    #+#             */
-/*   Updated: 2025/12/11 17:13:33 by gmach            ###   ########lyon.fr   */
+/*   Updated: 2025/12/11 17:55:27 by gmach            ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -91,22 +91,82 @@
 // 	return (0);
 // }
 
-int simple_sort(t_stack **stack_a, t_stack **stack_b)
+void	refill_a(t_stack **stack_a, t_stack **stack_b)
 {
-	int	size;
-	int	i;
-	int	j;
-	int	min;
+	while (*stack_b)
+		pa(stack_a, stack_b);
+}
 
-	size = ft_lstsize(*stack_a);
+int	simple_sort(t_stack **stack_a, t_stack **stack_b)
+{
+	int	size_a;
+	int	size_b;
+	int	i;
+	int	min_b;
+	int	max_b;
+
+	size_a = ft_lstsize(*stack_a);
 	i = 0;
-	if (size <= 1)
+	if (size_a <= 1)
 		return (-1);
+	//init 2 first terms in B
 	pb(stack_a, stack_b);
+	if ((*stack_a)->value > (*stack_b)->value)
+	{
+		min_b = (*stack_b)->value;
+		pb(stack_a, stack_b);
+		max_b = (*stack_b)->value;
+	}
+	else
+	{
+		min_b = (*stack_a)->value;
+		max_b = (*stack_b)->value;
+		pb(stack_a, stack_b);
+		sb(stack_b);
+	}
 	while (*stack_a)
 	{
-		if((*stack_a)->value > (*stack_b)->value)
-			pb()
+		size_b = ft_lstsize(*stack_b);
+		// if top a < min b
+		if ((*stack_a)->value < min_b)
+		{
+			set_min_b((*stack_a)->value, &min_b);
+			pb(stack_a, stack_b);
+			min_b = (*stack_b)->value;
+			print_stack(*stack_b, "B after pb");
+		}
+		// else if top a > max b
+		else if ((*stack_a)->value > max_b)
+		{
+			set_max_b((*stack_a)->value, &max_b);
+			pb(stack_a, stack_b);
+			max_b = (*stack_b)->next->value;
+			print_stack(*stack_b, "B after pb and rb");
+		}
+		else if((*stack_a)->value > (*stack_b)->value && (*stack_a)->value < ft_lstlast(*stack_b)->value)
+		{
+			pb(stack_a, stack_b);
+			print_stack(*stack_b, "B after pb");
+		}
+		else if ((*stack_a)->value > (*stack_b)->next->value)
+		{
+			pb(stack_a, stack_b);
+			sb(stack_b);
+			print_stack(*stack_b, "B after pb and sb");
+		}
+		else
+		{
+			while (size_b > 0 && (*stack_a)->value < (*stack_b)->value)
+			{
+				print_stack(*stack_a, "A");
+				print_stack(*stack_b, "B");
+				rb(stack_b);
+				size_b--;
+			}
+			pb(stack_a, stack_b);
+			print_stack(*stack_b, "B after pb");
+		}
 	}
+	refill_a(stack_a, stack_b);
 	return (0);
 }
