f7312ad836c84c73bb0d36577e62d41fc5d80e84
gmach <gmach@student.42lyon.fr>
2025-12-15 11:28:40 +0100

    norm push_swap.h

--- ft_push_swap.h
+++ ft_push_swap.h
@@ -6,7 +6,7 @@
 /*   By: gildas <gildas@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 07:14:40 by bfitte/gmac       #+#    #+#             */
-/*   Updated: 2025/12/15 11:06:20 by gildas           ###   ########lyon.fr   */
+/*   Updated: 2025/12/15 11:27:56 by gildas           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 # include <libft.h>
 
 /* Flags structure */
-typedef struct  s_flags
+typedef struct s_flags
 {
 	int	bench_mode;
 	int	simple;
@@ -38,7 +38,7 @@ typedef struct s_stack_ops
 	void	(*rotate)(t_stack **);
 	void	(*rrotate)(t_stack **);
 	void	(*swap)(t_stack **);
-}   t_stack_ops;
+}	t_stack_ops;
 
 /* Push_swap operations */
 void	sa(t_stack **stack_a);
@@ -54,16 +54,16 @@ void	rrb(t_stack **b);
 void	rrr(t_stack **a, t_stack **b);
 
 /* Utils functions */
-int	next_sqrt(int nb);
-int	find_min(t_stack *stack, int size);
-int	find_max(t_stack *stack, int size);
-void print_stack(t_stack *stack, char *name);
-int	count_nodes_until_value(t_stack *stack, int value);
+int		next_sqrt(int nb);
+int		find_min(t_stack *stack, int size);
+int		find_max(t_stack *stack, int size);
+void	print_stack(t_stack *stack, char *name);
+int		count_nodes_until_value(t_stack *stack, int value);
 t_stack_ops	get_stack_ops(t_stack **stack, char which);
-int	count_nodes_to_spot(t_stack_ops stack, int value, int to_sort);
-int	rot_top(t_stack_ops stack, int value);
-int	rot_bottom(t_stack_ops stack, int value);
-int	rot_spot(t_stack_ops stack, int value);
+int		count_nodes_to_spot(t_stack_ops stack, int value, int to_sort);
+int		rot_top(t_stack_ops stack, int value);
+int		rot_bottom(t_stack_ops stack, int value);
+int		rot_spot(t_stack_ops stack, int value);
 
 void	rel_rot_a_to_min(t_stack **stack_a, int target, int to_sort);
 int		rotate_b_to_spot(t_stack **stack_b, int value, int size_b);
