929ad238be1198fe01f77c9dc88768a37aaf0674
Bruno <bruno.fitte@sfr.fr>
2025-12-18 09:31:26 +0100

    Parsing ok

--- ft_push_swap_bonus.c
+++ ft_push_swap_bonus.c
@@ -6,7 +6,7 @@
 /*   By: bfitte <bfitte@student.42lyon.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 07:11:39 by bfitte/gmac       #+#    #+#             */
-/*   Updated: 2025/12/17 16:24:47 by bfitte           ###   ########lyon.fr   */
+/*   Updated: 2025/12/18 09:08:31 by bfitte           ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,25 +17,87 @@ void	delete_value(int value)
 	(void)value;
 }
 
+void	add_node(int value, t_stack **stack_a)
+{
+	t_stack	*new_node;
+	
+	new_node = ft_lstnew(value);
+	if (!new_node)
+	{
+		ft_lstclear(stack_a, delete_value);
+		return ;
+	}
+	ft_lstadd_back(stack_a, new_node);
+}
+
+int	check_input(char *nptr)
+{
+	int			i;
+	int			sign;
+	long long	result;
+
+	result = 0;
+	sign = 1;
+	i = 0;
+	while (nptr[i] == ' ' || (nptr[i] >= 9 && nptr[i] <= 13))
+		i++;
+	if (nptr[i] == '-' || nptr[i] == '+')
+	{
+		sign = -sign;
+		i++;
+	}
+	while (nptr[i])
+	{
+		if (!(nptr[i] <= 57 && nptr[i] >= 48))
+			return (1);
+		result = (result * 10 + (nptr[i] - 48)) * sign;
+		if (result > INT_MAX || result < INT_MIN)
+			return (1);
+		result *= sign;
+		i++;
+	}
+	return (0);
+}
+int	check_duplicate(int value, t_stack *stack_a)
+{
+	while (stack_a)
+	{
+		if (stack_a->value == value)
+			return (1);
+		stack_a = stack_a->next;
+	}
+	return (0);
+}
+
+t_stack	*handle_error(t_stack *stack_a)
+{
+	ft_lstclear(&stack_a, delete_value);
+	ft_printf("Error\n", 2);
+	return (stack_a);
+}
+
 t_stack	*parse_input(int nb_input, char **numbers)
 {
 	int		i;
 	t_stack	*stack_a;
-	t_stack	*new_node;
 	int		value;
 
 	stack_a = NULL;
 	i = 0;
 	while (i < nb_input)
 	{
-		value = ft_atoi(numbers[i]);
-		new_node = ft_lstnew(value);
-		if (!new_node)
+		if (check_input(numbers[i]) == 1)
+			i = -1;
+		else
 		{
-			ft_lstclear(&stack_a, delete_value);
-			return (NULL);
+			value = ft_atoi(numbers[i]);
+			if (check_duplicate(value, stack_a) == 1)
+				i = -1;
+			else
+				add_node(value, &stack_a);
 		}
-		ft_lstadd_back(&stack_a, new_node);
+		if (i == -1)
+			return (handle_error(stack_a));
 		i++;
 	}
 	return (stack_a);
